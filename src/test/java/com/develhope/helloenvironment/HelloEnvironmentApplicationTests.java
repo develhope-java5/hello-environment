08981e7a66e1520b614e569b457bb2ce7619d553
aegroto <aegroto@protonmail.com>
2022-10-26 14:37:45 +0200

    root

---
+++ src/test/java/com/develhope/helloenvironment/HelloEnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develhope.helloenvironment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloEnvironmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
